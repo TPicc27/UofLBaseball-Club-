7f6b433256e9548fa1b75b40d801a005f1c3fa8c
TPicc27 <anpicc01@cardmail.louisville.edu>
2017-07-20 16:00:50 -0400

    js on contact form, logo, footer width, readme file, and comments

---
+++ contactform.js
@@ -0,0 +1,38 @@
+//Form Validation
+function Validate() {
+//Get value from each input variable
+var name = document.forms["vform"]["name"].value;
+var email = document.forms["vform"]["email"].value;
+var phone = document.forms["vform"]["phone"].value;
+
+//If input values are not entered 
+if(name == null || name == "" || email == null || email == "" || phone == null || phone == "")
+    {
+        //Alert pop up message
+        alert("Please fill out your name, email, and phone");
+    }
+    //If input is entered
+    else {
+        if(name != null){
+            if(email != null){
+                if(phone != null) {
+                    //Alert Pop up message
+                    alert("Form was submitted");
+                }
+                else {
+                    //Alert pop up message
+                    alert("Enter phone number");
+                }
+            }
+            else {
+                //Alert pop up message 
+                alert("Enter a email address");
+            }
+        }
+        else {
+            //Alert pop up message
+            alert("Enter your name");
+        }
+    }
+}
+//End of Form Valdation 
---
+++ readme.md
@@ -0,0 +1,8 @@
+Overview:
+-------------
+My website is about my University of Louisville Club Baseball team. I want to be able to show what my team is all about, how you can contact or connect to us, how information about each player, stats on how they are doing in that current season. I also have a schedule so that people can come and watch us play. Add some Javascript to be able to tell the user if their form has been submitted or not.
+
+Viewing:
+----------
+You can open index.html file in your browser. As well as the schedule.html, roster.html, stats.html, and contact.html.  Once a html file is open you can easily maneuver through the site by clicking on one of the menu options.
+
